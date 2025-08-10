759dfe14d1a84ae397de2d0733116d0d34ec2ef1
Viktor Shadrin <v.viktor.shadrin@gmail.com>
2025-08-10 22:17:22 +0300

    3 tasks have been completed

--- src/category.py
+++ src/category.py
@@ -1,14 +1,32 @@
 class Category:
-    name: str
-    description: str
-    products: list
-    category_count = 0
-    product_count = 0
+	name: str
+	description: str
+	products: list
+	category_count = 0
+	product_count = 0
 
-    def __init__(self, name, description, products):
+	def __init__(self, name, description, products):
 
-        self.name = name
-        self.description = description
-        self.products = products
-        Category.category_count += 1
-        Category.product_count += len(products) if products else 0
+		self.name = name
+		self.description = description
+		self._products = products
+		Category.category_count += 1
+		Category.product_count += len(products) if products else 0
+
+
+	def add_product(self, new_product):
+		self.product_count += 1
+		self._products.append(new_product)
+
+
+
+	@property
+	def products(self):
+		return "".join(
+			f'{p.name}, {p.price} руб. Остаток: {p.quantity} шт.\n'
+			for p in self._products
+		)
+
+
+
+
--- src/product.py
+++ src/product.py
@@ -1,11 +1,16 @@
 class Product:
-    name: str
-    description: str
-    price: float
-    quantity: int
+	name: str
+	description: str
+	price: float
+	quantity: int
 
-    def __init__(self, name, description, price, quantity):
-        self.name = name
-        self.description = description
-        self.price = price
-        self.quantity = quantity
+	def __init__(self, name, description, price, quantity):
+		self.name = name
+		self.description = description
+		self.price = price
+		self.quantity = quantity
+
+	@classmethod
+	def new_product(cls, data):
+		return cls(**data)
+
