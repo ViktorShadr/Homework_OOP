92842e31767ed10f48de7baab3d59dac2dc00f3d
ViktorShadr <v.viktor.shadrin@gmail.com>
2025-08-11 17:19:00 +0300

    4 tasks have been complete

--- src/category.py
+++ src/category.py
@@ -1,31 +1,35 @@
 class Category:
-	name: str
-	description: str
-	products: list
-	category_count = 0
-	product_count = 0
+    name: str
+    description: str
+    products: list
+    category_count = 0
+    product_count = 0
 
-	def __init__(self, name, description, products):
+    def __init__(self, name, description, products):
 
-		self.name = name
-		self.description = description
-		self._products = products
-		Category.category_count += 1
-		Category.product_count += len(products) if products else 0
-
-
-	def add_product(self, new_product):
-		self.product_count += 1
-		self._products.append(new_product)
+        self.name = name
+        self.description = description
+        self._products = products
+        Category.category_count += 1
+        Category.product_count += len(products) if products else 0
 
 
+    def add_product(self, new_product):
+        self.product_count += 1
+        self._products.append(new_product)
 
-	@property
-	def products(self):
-		return "".join(
-			f'{p.name}, {p.price} руб. Остаток: {p.quantity} шт.\n'
-			for p in self._products
-		)
+
+    @property
+    def products(self):
+        return "".join(
+            f'{p.name}, {p.price} руб. Остаток: {p.quantity} шт.\n'
+            for p in self._products
+        )
+
+
+    @property
+    def products(self):
+        return self._products
 
 
 
--- src/product.py
+++ src/product.py
@@ -1,16 +1,45 @@
+from src.category import Category
+
+
 class Product:
-	name: str
-	description: str
-	price: float
-	quantity: int
+    name: str
+    description: str
+    price: float
+    quantity: int
 
-	def __init__(self, name, description, price, quantity):
-		self.name = name
-		self.description = description
-		self.price = price
-		self.quantity = quantity
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self._price = price
+        self.quantity = quantity
 
-	@classmethod
-	def new_product(cls, data):
-		return cls(**data)
+
+    @classmethod
+    def new_product(cls, data):
+        # Ищем товар с таким же именем
+        for product in Category.products:
+            if product.name.lower() == data["name"].lower():
+                # Суммируем количество
+                product.quantity += data["quantity"]
+                # Цена — максимальная
+                if data["price"] > product.price:
+                    product.price = data["price"]
+                return product  # Возвращаем обновленный товар
+
+        # Если не нашли — создаём новый
+        new_prod = cls(**data)
+        products_list.append(new_prod)
+        return new_prod
+
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, new_price):
+        if new_price <= 0:
+            print('Цена не должна быть нулевая или отрицательная')
+        else:
+            self._price = new_price
 
