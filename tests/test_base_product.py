c18d22582745999249182c120444bd2988fbb35b
ViktorShadr <v.viktor.shadrin@gmail.com>
2025-08-22 09:57:22 +0300

    Done all tests

---
+++ tests/test_base_product.py
@@ -0,0 +1,28 @@
+import pytest
+from src.base_product import BaseProduct
+
+
+def test_base_product_cannot_be_instantiated():
+    """Нельзя создать объект BaseProduct напрямую"""
+    with pytest.raises(TypeError):
+        BaseProduct()
+
+
+def test_inheritance_and_new_product():
+    """Проверяем, что наследник обязан реализовать new_product"""
+
+    class DummyProduct(BaseProduct):
+        def __init__(self, name, price):
+            self.name = name
+            self.price = price
+
+        @classmethod
+        def new_product(cls, name, price):
+            return cls(name, price)
+
+    # создаём через new_product
+    product = DummyProduct.new_product("Test product", 1000)
+
+    assert isinstance(product, DummyProduct)
+    assert product.name == "Test product"
+    assert product.price == 1000
